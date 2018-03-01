bbbfd7b5fbcf39248dd29b24f72518fb4e104c86
sundalei <sundalei2011@163.com>
2018-03-01 08:32:03 +0800

    helloworld

--- src/main/java/com/shengsiyuan/jdk8/Test3.java
+++ src/main/java/com/shengsiyuan/jdk8/Test3.java
@@ -1,8 +1,38 @@
 package com.shengsiyuan.jdk8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Test3 {
 
     public static void main(String[] args) {
 
+//        TheInterface i1 = () -> {};
+//        System.out.println(i1.getClass().getInterfaces()[0]);
+//
+//        TheInterface2 i2 = () -> {};
+//        System.out.println(i2.getClass().getInterfaces()[0]);
+//
+//        new Thread(() -> {
+//            System.out.println("hello world");
+//        }).start();
+
+
+        List<String> list = Arrays.asList("hello", "world", "hello world");
+//        list.forEach(item -> System.out.println(item.toUpperCase()));
+
+        List<String> list2 = new ArrayList<>();
+        list.forEach(item -> list2.add(item.toUpperCase()));
+        list2.forEach(System.out::println);
     }
+}
+
+@FunctionalInterface
+interface TheInterface {
+    void myMethod();
+}
+
+interface TheInterface2 {
+    void myMethod();
 }

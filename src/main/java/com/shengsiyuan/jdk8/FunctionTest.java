f40d65a7c64a241a908b694551a119154b8a55aa
sundalei <sundalei2011@163.com>
2018-03-05 21:24:05 +0800

    test

---
+++ src/main/java/com/shengsiyuan/jdk8/FunctionTest.java
@@ -0,0 +1,25 @@
+package com.shengsiyuan.jdk8;
+
+import java.util.function.Function;
+
+public class FunctionTest {
+
+    public static void main(String[] args) {
+        FunctionTest test = new FunctionTest();
+
+        System.out.println(test.compute(1, value -> {return 2 * value;}));
+        System.out.println(test.compute(2, value -> 5 + value));
+        System.out.println(test.compute(3, value -> value * value));
+
+        System.out.println(test.convert(5, value -> String.valueOf(value + "hello world")));
+    }
+
+    public int compute(int a, Function<Integer, Integer> function) {
+        int result = function.apply(a);
+        return result;
+    }
+
+    public String convert(int a, Function<Integer, String> function) {
+        return function.apply(a);
+    }
+}
--- src/main/java/com/shengsiyuan/jdk8/Test3.java
+++ src/main/java/com/shengsiyuan/jdk8/Test3.java
@@ -3,6 +3,7 @@ package com.shengsiyuan.jdk8;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class Test3 {
 
@@ -22,9 +23,15 @@ public class Test3 {
         List<String> list = Arrays.asList("hello", "world", "hello world");
 //        list.forEach(item -> System.out.println(item.toUpperCase()));
 
-        List<String> list2 = new ArrayList<>();
-        list.forEach(item -> list2.add(item.toUpperCase()));
-        list2.forEach(System.out::println);
+//        List<String> list2 = new ArrayList<>();
+//        list.forEach(item -> list2.add(item.toUpperCase()));
+//        list2.forEach(System.out::println);
+
+//        list.stream().map(item -> item.toUpperCase()).forEach(item -> System.out.println(item));
+//        list.stream().map(String::toUpperCase).forEach(item -> System.out.println(item));
+
+        Function<String, String> function = String::toUpperCase;
+        System.out.println(function.getClass().getInterfaces()[0]);
     }
 }
 

a9ef3717a7d4ff3b93ec7cc64b9839910288c3af
孙大雷 <sundalei@192.168.0.102>
2018-03-18 08:03:20 +0800

    test

---
+++ src/main/java/com/shengsiyuan/jdk8/stream/StreamTest10.java
@@ -0,0 +1,14 @@
+package com.shengsiyuan.jdk8.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamTest10 {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("hello", "world", "hello world");
+
+        list.stream().mapToInt(item -> item.length()).filter(length -> length == 5)
+                .findFirst().ifPresent(System.out::println);
+    }
+}

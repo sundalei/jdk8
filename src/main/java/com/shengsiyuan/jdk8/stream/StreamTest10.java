6818e3e38fcc170946168f74b594e46d4b78ac3c
孙大雷 <sundalei@192.168.0.101>
2018-03-19 21:02:44 +0800

    test

--- src/main/java/com/shengsiyuan/jdk8/stream/StreamTest10.java
+++ src/main/java/com/shengsiyuan/jdk8/stream/StreamTest10.java
@@ -6,9 +6,15 @@ import java.util.List;
 public class StreamTest10 {
 
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("hello", "world", "hello world");
+        List<String> list = Arrays.asList("hello1", "world1", "hello world");
 
-        list.stream().mapToInt(item -> item.length()).filter(length -> length == 5)
-                .findFirst().ifPresent(System.out::println);
+//        list.stream().mapToInt(item -> item.length()).filter(length -> length == 5)
+//                .findFirst().ifPresent(System.out::println);
+
+        list.stream().mapToInt(item -> {
+            int length = item.length();
+            System.out.println(item);
+            return length;
+        }).filter(length -> length == 5).findFirst().ifPresent(System.out::println);
     }
 }

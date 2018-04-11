236365aa94a14a47b306ad4cfd836f89d1d011d5
孙大雷 <sundalei@192.168.0.103>
2018-04-11 23:02:55 +0800

    AutoCloseableTest

---
+++ src/main/java/com/shengsiyuan/jdk8/stream2/AutoCloseableTest.java
@@ -0,0 +1,20 @@
+package com.shengsiyuan.jdk8.stream2;
+
+public class AutoCloseableTest implements AutoCloseable {
+
+    public static void main(String[] args) throws Exception {
+
+        try(AutoCloseableTest autoCloseableTest = new AutoCloseableTest()) {
+            autoCloseableTest.doSomething();
+        }
+    }
+
+    public void doSomething() {
+        System.out.println("doSomething invoked!");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("close invoked!");
+    }
+}
